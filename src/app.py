6127a1d0bd0670e5cd15e7b1e41734ead7cc61f4
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-10-31 13:21:17 +0200

    Make corrections to app.py and index.html

--- src/app.py
+++ src/app.py
@@ -1,15 +1,26 @@
 from flask import Flask
-from flask import abort, flash, make_response, redirect, render_template, request
+from flask import abort, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Palauttaa sivun index.html
+    Returns:
+        index.html sivu
+    """
 
     return render_template("index.html")
     
 @app.route("/check_typos", methods=["POST"])
 def check_typos():
+    """Saa käyttäjältä syötteenä tarkistettavan sanan
+    ja tarkistaa oikeinkirjoituksen
+    Returns:
+        palauttaa sivun correct.html, joka sisältää
+    arvion sanan oikeinkirjoituksesta
+    """
+
     word = request.form["word"]
     if not word or len(word) > 25:
         abort(403)
