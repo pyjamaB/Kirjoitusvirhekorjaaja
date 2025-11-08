1cd76419f5c54d4517eb3e24a1a810e13f3155a9
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-11-08 10:46:44 +0200

    Add first unittests

---
+++ src/tests/test_trie.py
@@ -0,0 +1,28 @@
+import unittest
+from trie import Trie
+
+class TestTrie(unittest.TestCase):
+    def setUp(self):
+        self.trie = Trie()
+        self.trie.add_word("alfa")
+        self.trie.add_word("beeta")
+        self.trie.add_word("gamma")
+        self.trie.add_word("yksi")
+        self.trie.add_word("kaksi")
+        self.trie.add_word("kolme")        
+
+    def test_add_to_trie(self):
+        self.assertEqual(True, self.trie.search_word("alfa"))
+        self.assertEqual(True, self.trie.search_word("beeta"))
+        self.assertEqual(True, self.trie.search_word("gamma"))
+        self.assertEqual(True, self.trie.search_word("yksi"))
+        self.assertEqual(True, self.trie.search_word("kaksi"))
+        self.assertEqual(True, self.trie.search_word("kolme"))
+
+    def test_search_from_trie(self):   
+        self.assertEqual(True, self.trie.search_word("alfa"))
+        self.assertEqual(True, self.trie.search_word("beeta"))
+        self.assertEqual(True, self.trie.search_word("gamma"))
+        self.assertEqual(True, self.trie.search_word("yksi"))
+        self.assertEqual(True, self.trie.search_word("kaksi"))
+        self.assertEqual(True, self.trie.search_word("kolme"))
