cae644d79447ea66e6b2038e74157e5478bd5ca8
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-11-08 09:55:40 +0200

    Add docstring to vocabulary.py

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -1,6 +1,13 @@
 import xml.etree.ElementTree as ET
 
 def parse_words(xml_path):
+    """Metodi joka poimii sanastotietokannan sanat xml-tiedostosta,
+    ja palauttaa ne trie-tietorakenteeseen lisäämistä varten.
+    Args:
+        xml_path: xml-tiedoston sijainti hakemistossa.
+    Returns:
+        words: lista tietokannan sanoista.
+    """
     tree = ET.parse(xml_path)
     root = tree.getroot()
     words = []
