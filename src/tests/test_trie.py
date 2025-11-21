72e2d5f7dfa4809990a731cf451bd067183620c4
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-11-21 09:49:45 +0200

    Add hypothesis to project tests and modify test_trie.py

--- src/tests/test_trie.py
+++ src/tests/test_trie.py
@@ -1,3 +1,5 @@
+import hypothesis.strategies as st
+from hypothesis import given, settings
 import unittest
 from trie import Trie
 
@@ -28,3 +30,10 @@ class TestTrie(unittest.TestCase):
         self.assertEqual(True, self.trie.search_word("kolme"))
         self.assertEqual(False, self.trie.search_word("neljä"))
         self.assertEqual(False, self.trie.search_word(""))
+
+    @given(arvo=st.text(alphabet="abcdefghijklmnopqrstyvwxyzåäö", min_size=1, max_size=35))    
+    @settings(max_examples=500)
+    def test_trie_hypothesis(self, arvo):
+        h_trie = Trie()
+        h_trie.add_word(arvo)
+        assert h_trie.search_word(arvo)
