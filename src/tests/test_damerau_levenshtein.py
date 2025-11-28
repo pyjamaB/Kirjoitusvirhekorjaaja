e75ff514b7e03a3bac49bc080e39a6fd440bf257
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-11-28 14:01:12 +0200

    Make changes to test_damerau_levenshtein.py

--- src/tests/test_damerau_levenshtein.py
+++ src/tests/test_damerau_levenshtein.py
@@ -4,6 +4,7 @@ from damerau_levenshtein import damerau_levenshtein
 class TestDamerauLevenshtein(unittest.TestCase):
     def test_damerau_levenshtein(self):
         self.assertEqual(1, damerau_levenshtein("testi", "testit"))
+        self.assertEqual(1, damerau_levenshtein("testi", "test"))
         self.assertEqual(0, damerau_levenshtein("testi", "testi"))
         self.assertEqual(5, damerau_levenshtein("", "testi"))
         self.assertEqual(0.9, damerau_levenshtein("tetsi", "testi"))
