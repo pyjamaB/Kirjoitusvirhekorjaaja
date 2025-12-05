58676e9c92d2e1bf438ba9ad6074fccd714c68c9
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-12-05 10:08:09 +0200

    Make changes to test_damerau_levenshtein.py

--- src/tests/test_damerau_levenshtein.py
+++ src/tests/test_damerau_levenshtein.py
@@ -2,14 +2,25 @@ import unittest
 from damerau_levenshtein import damerau_levenshtein
 
 class TestDamerauLevenshtein(unittest.TestCase):
-    def test_damerau_levenshtein(self):
+    def test_damerau_levenshtein_add_letter(self):
         self.assertEqual(1, damerau_levenshtein("testi", "testit"))
+    def test_damerau_levenshtein_remove_letter(self):
         self.assertEqual(1, damerau_levenshtein("testi", "test"))
+    def test_damerau_levenshtein_same_words(self):
         self.assertEqual(0, damerau_levenshtein("testi", "testi"))
+    def test_damerau_levenshtein_empty_string(self):
         self.assertEqual(5, damerau_levenshtein("", "testi"))
+    def test_damerau_levenshtein_transposition(self):
         self.assertEqual(0.9, damerau_levenshtein("tetsi", "testi"))
+    def test_damerau_levenshtein_add_letter_change_order(self):
         self.assertEqual(1, damerau_levenshtein("testit", "testi"))
+    def test_damerau_levenshtein_replace_letter(self):
         self.assertEqual(1.4, damerau_levenshtein("tahti", "tuhti"))
+    def test_damerau_levenshtein_replace_letter_unicode(self):
         self.assertEqual(1.7, damerau_levenshtein("tähti", "tahti"))
+    def test_damerau_levenshtein_add_letter_transposition(self):
         self.assertEqual(1.7, damerau_levenshtein("testi", "etstit"))
+    def test_damerau_levenshtein_add_letter_transpose_replace(self):
         self.assertEqual(2.4, damerau_levenshtein("abcdefg", "tabcedfh"))
+    def test_damerau_levenshtein_add_letter_remove(self):
+        self.assertEqual(2, damerau_levenshtein("abcdefg", "tabcdef"))
