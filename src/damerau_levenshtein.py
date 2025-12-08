58df78ecd788b4c7804863dd6c273587fbc57d92
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-12-08 16:53:50 +0200

    Make changes to damerau_levenshtein.py

--- src/damerau_levenshtein.py
+++ src/damerau_levenshtein.py
@@ -1,4 +1,3 @@
-
 def weighted_cost(key1, key2, base_cost):
     """Funktio, joka laskee ensin kahden merkin välisen etäisyyden
     näppäimistöllä ja laskee etäisyyden perusteella painon transpoosille
@@ -44,12 +43,12 @@ def damerau_levenshtein(word1, word2):
             if word1[i - 1] == word2[j - 1]:
                 cost = 0
             else:
-                cost = weighted_cost(word1[i - 1], word2[j - 1],  0.7)
+                cost = weighted_cost(word1[i - 1], word2[j - 1], 0.7)
             distance[i][j] = min(distance[i - 1][j] + 1,
                                  distance[i][j - 1] + 1,
                                  distance[i - 1][j - 1] + cost)
             if i > 1 and j > 1 and word1[i - 1] == word2[j - 2] and word1[i - 2] == word2[j - 1]:
                 distance[i][j] = min(distance[i][j],
                                      distance[i - 2][j - 2] +
-                                     weighted_cost(word1[i - 2], word1[i - 1], 0.5))
+                                     weighted_cost(word1[i - 1], word2[j - 1], 0.5))
     return distance[len(word1)][len(word2)]
